701748c014d6697c351e4cdb4580ab6f57a383f2
zystudios <zystudios@gmail.com>
2025-10-30 14:57:50 +0800

    Update index.tsx

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -7,53 +7,63 @@
  * @Description:
  */
 
-import { Avatar, Button, message, Spin } from "antd"
-import { useEffect, useState } from "react"
+import {
+  Alert,
+  Avatar,
+  Button,
+  ConfigProvider,
+  message,
+  Select,
+  Spin,
+  Switch,
+} from "antd";
+import { useEffect, useState } from "react";
 
-import "./index.less"
+import "./index.less";
 
-import { SettingOutlined, SyncOutlined } from "@ant-design/icons"
+import { SettingOutlined, SyncOutlined } from "@ant-design/icons";
 
-import { Storage } from "@plasmohq/storage"
+import { Storage } from "@plasmohq/storage";
+import { appVerion } from "~config";
 
-const storage = new Storage({ area: "local" })
+const storage = new Storage({ area: "local" });
 
 function IndexPopup() {
   if (process.env.NODE_ENV == "production") {
-    window.console.log = (param: any) => {}
-    window.console.error = (param: any) => {}
-    window.console.warn = (param: any) => {}
-    window.console.debug = (param: any) => {}
+    window.console.log = (param: any) => {};
+    window.console.error = (param: any) => {};
+    window.console.warn = (param: any) => {};
+    window.console.debug = (param: any) => {};
   }
 
-  const [loading, setLoading] = useState(false)
-
-  const [bg, setBg] = useState<any>("")
+  const [loading, setLoading] = useState(false);
+  const [awake, setAwake] = useState(false);
+  const [bg, setBg] = useState<any>("");
   const [messageApi, contextHolder] = message.useMessage({
     top: "60px",
-    duration: 2
-  })
+    duration: 2,
+  });
   useEffect(() => {
-    setBg(generateBg())
+    setBg(generateBg());
     const init = async () => {
       try {
       } catch {}
-    }
-    init()
-  }, [])
+    };
+    init();
+  }, []);
 
   const randomNum = (min: number, max: number) => {
-    const minCeiled = Math.ceil(min)
-    const maxFloored = Math.floor(max)
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
-  }
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  };
   //radial-gradient(shape size at position, start-color, ..., last-color); hsla(hue, saturation, lightness, alpha)
   const generateBg = () => {
     return `radial-gradient(at ${randomNum(20, 40)}% ${randomNum(30, 80)}%, hsla(0, 100%, ${randomNum(70, 100)}%, 0.1) 0, hsla(114, 100%, 100%, 0) 40%),
             radial-gradient(at ${randomNum(50, 70)}% ${randomNum(30, 80)}%, hsla(201, 100%, ${randomNum(70, 100)}%, 0.1) 0, hsla(201, 100%, 100%, 0) 40%),        
             radial-gradient(at ${randomNum(70, 100)}% ${randomNum(30, 80)}%, hsla(112, 100%, ${randomNum(70, 100)}%, 0.1) 0, hsla(112, 100%, 100%, 0) 40%)
-            `
-  }
+            `;
+  };
   return (
     <div className="layout">
       <Spin fullscreen spinning={loading} size="large"></Spin>
@@ -62,57 +72,107 @@ function IndexPopup() {
         className="content"
         style={{
           backgroundColor: "#fff",
-          backgroundImage: bg
-        }}>
+          backgroundImage: bg,
+        }}
+      >
         <div
           style={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "space-between",
-            margin: "5px 0 10px 0"
-          }}>
+            justifyContent: "center",
+          }}
+        >
+          <img width={36} src={require("data-base64:../../assets/icon.png")} />
+          <span
+            style={{
+              textAlign: "center",
+              marginLeft: 5,
+              backgroundImage: "linear-gradient(to right, #0090ff, #ff65ff)",
+              color: "transparent",
+              WebkitBackgroundClip: "text",
+              fontWeight: 600,
+              fontSize: 20,
+            }}
+          >
+            Keep Awake
+          </span>
+        </div>
+
+        <></>
+        <div style={{ textAlign: "center", marginTop: 20 }}>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: "#1abb6b",
+              },
+            }}
+          >
+            <Switch
+              value={awake}
+              onChange={(v) => {
+                setAwake(v);
+              }}
+            ></Switch>
+          </ConfigProvider>
           <div
             style={{
-              display: "flex",
-              alignItems: "center"
-            }}>
-            <img
-              width={36}
-              src={require("data-base64:../../assets/icon.png")}
-            />
-            <span
-              style={{
-                textAlign: "center",
-                marginLeft: 5,
-                backgroundImage: "linear-gradient(to right, #0090ff, #ff65ff)",
-                color: "transparent",
-                WebkitBackgroundClip: "text",
-                fontWeight: 600
-              }}>
-              Demo
-              <br />
-              Extension
-            </span>
+              fontSize: 14,
+              fontWeight: 600,
+              marginTop: 20,
+              color: awake ? "#1abb6b" : "#ff4c50",
+            }}
+          >
+            {awake ? "Keep Awake Enabled" : "Keep Awake Disabled"}
           </div>
-
-          <div>
-            <Button
-              type="primary"
-              style={{ marginLeft: 10 }}
-              onClick={() => {}}>
-              <SyncOutlined />
-            </Button>
+          <div style={{ marginTop: 10, fontSize: 14 }}>
+            Disable After{" "}
+            <Select
+              size="small"
+              listHeight={130}
+              defaultValue={0}
+              style={{ width: 80 }}
+              options={[
+                { label: "OFF", value: 0 },
+                { label: "10 m", value: 10 },
+                { label: "20 m", value: 20 },
+                { label: "30 m", value: 30 },
+                { label: "40 m", value: 40 },
+                { label: "50 m", value: 50 },
+                { label: "1 h", value: 60 },
+                { label: "2 h", value: 120 },
+                { label: "3 h", value: 180 },
+                { label: "4 h", value: 240 },
+                { label: "5 h", value: 300 },
+                { label: "6 h", value: 360 },
+                { label: "7 h", value: 420 },
+                { label: "8 h", value: 480 },
+                { label: "9 h", value: 540 },
+                { label: "10 h", value: 600 },
+                { label: "11 h", value: 660 },
+                { label: "12 h", value: 720 },
+              ]}
+            ></Select>
           </div>
-          <Button onClick={() => {}}>
-            <SettingOutlined />
-          </Button>
+          <Alert
+            style={{ marginTop: 20 }}
+            type="info"
+            message={
+              "Keeping the screen awake will prevent the computer from turning off or going to sleep."
+            }
+          ></Alert>
         </div>
       </div>
-      <div className="footer"></div>
+      <div
+        className="footer"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <div>Disable: {"00:00:00"}</div>
+        <div>Ver: {appVerion}</div>
+      </div>
 
       {contextHolder}
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
