0741b6d215e6d28c90869b4d9c5830e129683066
zystudios <zystudios@gmail.com>
2025-10-30 16:43:45 +0800

    Create background.ts

---
+++ src/background.ts
@@ -0,0 +1,60 @@
+/*
+ * @Author: zhangyan
+ * @Date: 2023-11-15 19:48:54
+ * @LastEditTime: 2024-11-20 19:55:16
+ * @LastEditors: zhangyan
+ * @FilePath: /website-cookie-sync-extension/src/background.ts
+ * @Description:
+ */
+
+import { keepAlive } from "@plasmohq/persistent/background";
+import { Storage } from "@plasmohq/storage";
+
+const storage = new Storage({
+  area: "local",
+});
+
+export {};
+
+if (process.env.NODE_ENV == "production") {
+  console.log = (param: any) => {};
+  console.error = (param: any) => {};
+  console.warn = (param: any) => {};
+  console.debug = (param: any) => {};
+}
+
+keepAlive();
+let loop_count = 0;
+
+setInterval(async () => {
+  try {
+    const awake = (await storage.get("awake")) || "0";
+    const disable_time = (await storage.get("disable")) || "0";
+
+    if (awake == "1") {
+      chrome.power.requestKeepAwake("display");
+
+      if (disable_time != "0") {
+        loop_count++;
+        chrome.runtime.sendMessage({
+          type: "count_down",
+          value: Number(disable_time) * 60 - loop_count,
+        });
+        if (loop_count >= Number(disable_time) * 60) {
+          loop_count = 0;
+          chrome.power.releaseKeepAwake();
+          await storage.set("awake", "0");
+          await storage.set("disable", "0");
+        }
+      }
+    }
+  } catch {}
+}, 1000);
+
+// // 特别注意，默认同步，要改异步必须这么写
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+  if (req.type == "reset_time") {
+    loop_count = 0;
+    return true;
+  }
+});
