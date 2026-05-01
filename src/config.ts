677908c6a108cb22e26959f78919ca10c1039aa9
zystudios <zystudios@gmail.com>
2026-05-01 16:44:36 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: zhangyan
  * @Date: 2025-09-08 23:12:52
- * @LastEditTime: 2025-10-31 21:12:42
+ * @LastEditTime: 2026-05-01 16:36:11
  * @LastEditors: zhangyan
  * @FilePath: /chrome-keep-awake-extension/src/config.ts
  * @Description:
  */
 
-export const appVerion = "1.0.1";
+export const appVerion = "1.0.2";
